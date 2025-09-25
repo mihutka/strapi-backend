e19bbbf9a05f78133ccb531d3fd61c9d78e86e5f
Mihhail <misabajandinov@gmail.com>
2025-09-25 06:47:28 +0300

    update

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -3,13 +3,17 @@ export default [
   {
     name: 'strapi::cors',
     config: {
+      // ВАРИАНТ А: точные домены (без слэшей!)
       origin: [
         'http://localhost:3000',
-        'https://strapi-frontend-eweb-2t9b8ymht-misabajandinov-5741s-projects.vercel.app',
+        'https://strapi-frontend-eweb-git-main-misabajandinov-5741s-projects.vercel.app/',
+        // можешь добавить стабильный превью-домен проекта, если есть
+        // 'https://strapi-frontend-eweb.vercel.app',
       ],
       methods: ['GET','POST','PUT','PATCH','DELETE','HEAD','OPTIONS'],
       headers: ['Content-Type','Authorization','Origin','Accept'],
-      credentials: true,
+      keepHeaderOnError: true, // отдать CORS даже при 4xx/5xx, иначе браузер тоже ругается
+      credentials: false,      // оставь false, если не используешь куки с фронта
     },
   },
   'strapi::security',
