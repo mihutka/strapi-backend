6fd33dabf452c6e19c9a854a0864914277773b83
Mihhail <misabajandinov@gmail.com>
2025-09-25 05:03:11 +0300

    fix: add server config with app.keys

--- config/server.ts
+++ config/server.ts
@@ -1,6 +1,7 @@
 export default ({ env }) => ({
-  host: '0.0.0.0',
+  host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: env('PUBLIC_URL'), // позже подставим URL render-сервиса
-  app: { keys: env.array('APP_KEYS') },
+  app: {
+    keys: env.array('APP_KEYS'),
+  },
 });
